d738737fc5d74fe156274cec3871fe51a5b52092
HobbyNoobyCoder <erenmotog5@gmail.com>
2019-03-07 23:37:38 +0530

    LinkedList Started and something Done

--- VS_Datastructures_in_CPP/First_File/LinkedList.cpp
+++ VS_Datastructures_in_CPP/First_File/LinkedList.cpp
@@ -1,9 +1,43 @@
 #include "stdafx.h"
 #include<iostream>
 using namespace std;
+/*
+struct Node {
+	int data; 
+	struct Node* Next;
+} Node;
+*/
+
+typedef struct {
+	int data;
+	struct Node* Next;
+};
+typedef struct Node* NodePtr;
 
 int main()
 {
-	cout << "Lets begin with Learning and Practicing of DS!!!!\n";
+	char enteredData[25];
+	 
+	while (1)
+	{
+		cout << "Entered Data is before " << enteredData;
+		cout << "Enter a Number to be stored or type end to exit \n";
+		cin >> enteredData;
+		try {
+			if (enteredData == "end")
+				throw enteredData;
+			else
+			{
+				insertData(enteredData);
+			}
+		}
+		catch (char*)
+		{
+			cout<<"The Entered list of Data are as follows\n"
+			DisplayData
+		}
+		cout << "Entered Data is " << enteredData;
+	}
+	
 	return 0;
 }
