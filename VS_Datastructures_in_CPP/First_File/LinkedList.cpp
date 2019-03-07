9b71cb650250c32b4f5f8bb6fff493a0e63ad297
HobbyNoobyCoder <erenmotog5@gmail.com>
2019-03-07 19:31:01 +0530

    LinkedList file Edited Commit

--- VS_Datastructures_in_CPP/First_File/LinkedList.cpp
+++ VS_Datastructures_in_CPP/First_File/LinkedList.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-	cout << "What the Fuck??? Nope I will Fuck Everything He he \n";
+	cout << "What the Fuck??? Nope I will Fuck Everything He he \n That \n is \n for \n sure !!!!";
 	return 0;
 }
