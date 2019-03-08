52e8ac02947a1eaa0e8ff67c697da79be05f2b36
HobbyNoobyCoder <erenmotog5@gmail.com>
2019-03-08 08:35:58 +0530

    LinkedList half Done

--- VS_Datastructures_in_CPP/First_File/LinkedList.cpp
+++ VS_Datastructures_in_CPP/First_File/LinkedList.cpp
@@ -8,16 +8,54 @@ struct Node {
 } Node;
 */
 
-typedef struct {
+struct Node {
 	int data;
 	struct Node* Next;
 };
 typedef struct Node* NodePtr;
 
+NodePtr createNode(NodePtr);
+void insertData(NodePtr, char[]);
+void DisplayDataAsQueue(NodePtr, int);
+
+void insertData(NodePtr nodePtr, char data[])
+{
+	if (nodePtr == NULL)
+	{
+		createNode(nodePtr);
+		nodePtr->data = (int)(*data);
+		nodePtr->Next = NULL;
+	}
+	else
+	{
+		insertData(nodePtr->Next, data);
+	}
+
+}
+
+void DisplayDataAsQueue(NodePtr nodePtr, int level=1)
+{
+	if (nodePtr != NULL) {
+		cout << "Element No " << level << "is" << nodePtr->data;
+		DisplayDataAsQueue(nodePtr->Next, ++level);
+	}
+}
+
+NodePtr createNode(NodePtr node)
+{
+	node = (NodePtr)malloc(1 * sizeof(struct Node));
+	if (node == NULL)
+	{
+		cout << "\n Memory Error , Exiting";
+		exit(1);
+	}
+}
+
+
 int main()
 {
-	char enteredData[25];
-	 
+	char enteredData[25] = {};
+	Node NodeLinkedList;
 	while (1)
 	{
 		cout << "Entered Data is before " << enteredData;
@@ -28,15 +66,16 @@ int main()
 				throw enteredData;
 			else
 			{
-				insertData(enteredData);
+				insertData(&NodeLinkedList,enteredData);
 			}
 		}
 		catch (char*)
 		{
-			cout<<"The Entered list of Data are as follows\n"
-			DisplayData
+			cout << "The Entered list of Data are as follows\n";
+				DisplayDataAsQueue(&NodeLinkedList);
+			return 0;
 		}
-		cout << "Entered Data is " << enteredData;
+		
 	}
 	
 	return 0;
