11db19fadc133c500a42d16677dfbb1bad1c0203
HoobyNoobyCoder <erernmotog5@gmail.com>
2019-03-09 11:41:50 +0530

    Rebooted LinkedList v1 done

--- VS_Datastructures_in_CPP/First_File/LinkedList.cpp
+++ VS_Datastructures_in_CPP/First_File/LinkedList.cpp
@@ -1,87 +1,65 @@
 #include "stdafx.h"
 #include<iostream>
 using namespace std;
-/*
 struct Node {
-	int data; 
-	struct Node* Next;
-} Node;
-*/
-
-struct Node {
-	int data;
-	struct Node* Next=NULL;
+	int nodeData;
+	struct Node* next;
 };
-typedef struct Node* NodePtr;
-
-void createNode(NodePtr*);
-void insertData(NodePtr, char[]);
-void DisplayDataAsQueue(NodePtr, int);
-
-void insertData(NodePtr nodePtr, char data[])
-{
-	if (nodePtr == NULL)
-	{
-		createNode(&nodePtr);
-		nodePtr->data = (int)(*data);
-		nodePtr->Next = NULL;
-	}
-	else
-	{
-		insertData(nodePtr->Next, data);
-	}
-
-}
-
-void DisplayDataAsQueue(NodePtr nodePtr, int level=1)
-{
-	if (nodePtr != NULL) {
-		cout << "Element No " << level << "is" << nodePtr->data;
-		DisplayDataAsQueue(nodePtr->Next, ++level);
-	}
-}
-
-void createNode(NodePtr* node)
-{
-	*node = (NodePtr)malloc(1 * sizeof(struct Node));
-	if (*node == NULL)
-	{
-		cout << "\n Memory Error , Exiting";
-		exit(1);
-	}
-
-}
-
+struct Node* Head = NULL;
+void insertListData(struct Node**, int data);
+void displayListData(struct Node*,int level=1);
+struct Node* createNode();
 
 int main()
 {
-	char enteredData[25] = {};
-	Node NodeLinkedList;
-	int record;
-	cout << "Enter the number of records to be stored\n";
-	cin >> record;
-	for(int i=0;i<record;i++)
-	{
-		cout << "Entered Data is before " << enteredData;
-		cout << "Enter a Number to be stored or type end to exit \n";
-		cin >> enteredData;
-		try {
-			if (enteredData == "end")
-				throw enteredData;
-			else
-			{
-				insertData(&NodeLinkedList,enteredData);
-			}
-		}
-		catch (char*)
-		{
-			cout << "The Entered list of Data are as follows\n";
-				DisplayDataAsQueue(&NodeLinkedList);
-			return 0;
-		}
-		
-	}
-	cout << "The Entered list of Data are as follows\n";
-	DisplayDataAsQueue(&NodeLinkedList);
+	std::cout << "Program started \n";
+	insertListData(&Head, 1);
+	insertListData(&Head, 2);
+	insertListData(&Head, 3);
+	insertListData(&Head, 4);
+	cout << "Inserting of Data Done \n";
+
+	displayListData(Head);
+
+	cout << "End of program\n";
 	return 0;
+}
+
+void insertListData(struct Node** head, int data)
+{
+	if (*head == NULL)
+	{
+		*head = createNode();
+		(*head)->nodeData = data;
+		(*head)->next = NULL;
+	}
+	else
+	{
+		insertListData(&((*head)->next), data);
+	}
+
+}
+
+struct Node* createNode()
+{
+	struct Node* new_node = (struct Node*)malloc(1 * sizeof(struct Node));
+	if (new_node == NULL)
+	{
+		cout << "Memory Error, Exiting application\n";
+		exit (1);
+	}
+	return new_node;
+}
+
+void displayListData(struct Node* nodePtr, int level)
+{
+	if (nodePtr != NULL)
+	{
+		displayListData(nodePtr->next, level+1);
+		cout << "The element in Node " << level << " is " << nodePtr->nodeData<<"\n";
+	}
+	else
+	{
+		cout << "\n Reached Null Node\n";
+	}
 }
