db5ede5c57cc0b826da16ab5dabe68d117c660b9
HoobyNoobyCoder <erenmotog5@gmail.com>
2019-03-09 20:36:49 +0530

    working basic Stack using linkedlist

--- VS_Datastructures_in_CPP/First_File/StackUsingLinkedList.cpp
+++ VS_Datastructures_in_CPP/First_File/StackUsingLinkedList.cpp
@@ -1,146 +1,137 @@
 #include "stdafx.h"
 #include<iostream>
 using namespace std;
-/*=====Start of Node Structure Defination=====*/
 
-struct Node
-{
-	int nodeData;
-	struct Node* nextNodePtr;
+class myStack {
+/* Structure for Linked List Node*/
+	struct Node
+	{
+		int nodeData;
+		struct Node* nextNodePtr=NULL;
+	};
+	typedef struct Node* NodePtr;
+
+public:
+/*Function Prototypes*/
+//	int pop();
+//	void push(int data);
+//	int peek();
+//	bool isEmpty();
+//	NodePtr createNode();
+
+/* Declarations */
+
+	NodePtr Top = NULL;
+/*Functions Definations*/
+
+/*Funtions to pop the top element from the stack*/
+	int pop()
+	{
+		if (!isEmpty())
+		{
+			int data;
+			NodePtr temp = Top;
+			Top = Top->nextNodePtr;
+			data = temp->nodeData;
+			free(temp);
+			return data;
+		}
+		else
+		{
+			cout << "Stack underflow, exiting program" << endl;
+			exit(1);
+		}
+	}
+/*Function to push the Data to the Stack*/
+	void push(int data)
+	{
+		if (isEmpty())
+		{
+			Top = createNode();
+			Top->nodeData = data;
+		}
+		else
+		{
+			NodePtr new_node = createNode();
+			new_node->nodeData = data;
+			new_node->nextNodePtr = Top;
+			Top = new_node;
+		}
+
+	}
+
+/*Function to check whether the stack is empty or not*/
+	bool isEmpty()
+	{
+		return(Top == NULL);
+	}
+/*Function to get the Top element without deleting it */
+	int peek()
+ {
+	 if (isEmpty())
+		{
+		 cout << "Empty Stack, No element is pushed" << endl;
+		 return -1;
+		}
+	 else
+		 {
+		 return (Top->nodeData);
+		 }
+ }
+/*Function to Create new Node */
+	NodePtr createNode()
+	{
+		NodePtr new_node = (NodePtr)malloc(1 * (sizeof(struct Node)));
+		if (new_node == NULL)
+		{
+			cout << "Memory Error in Malloc" << endl;
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			new_node->nextNodePtr = NULL;
+			return new_node;
+		}
+	}
 };
-
-typedef struct Node* NodePtr;
-/*=====End of Node Structure Defination=====*/
-
-/*=====Start of myStack Structure =====*/
-struct myStack
-{
-	NodePtr stackNode=NULL;
-	int stackSize = 0;
-};
-
-myStack stack;
-/*=====End of myStack Structure =====*/
-
-/*=====Start of function Prototypes=====*/
-int  pop();
-void push(int data);
-int  peek();
-bool isEmpty();
-void deleteStack();
-void displayStack(NodePtr nodePtr=stack.stackNode);
-/*=====End of function Prototypes=====*/
-
-/*=====Start of Main =====*/
+ char instructionString[] = "Choose \n '1' to Push \n '2' to Pop \n '3' to Peek \n '4' to exit \n";
 int main()
 {
+	myStack stack;
 	int option = 0;
-	
-	while (option != 4) {
-		cout << "Enter \n '1' to Push \n '2' to Pop\n '3' to Check for peek element\n ";
-		cout << "'4' to end the program\n";
+	while (option != 4)
+	{
+		cout << instructionString << endl;
 		cin >> option;
+
 		switch (option)
 		{
 		case 1:
-			int enteredData;
-			cout << "Enter the data to be pushed" << endl;
-			cin >> enteredData;
-			push(enteredData);
+			int data;
+			cout << "Enter the number to be pushed" << endl;
+			cin>> data;
+			stack.push(data);
+			cout << data << " was pushed" << endl;
 			break;
+
 		case 2:
-			cout << "The poped out Element is :" << endl << pop();
+			cout << "The popped out Number is:" << endl;
+			cout << stack.pop()<<endl;
 			break;
 
 		case 3:
-			cout << "The Last pushed element is :" << endl << peek();
+			cout << "The top element is :" << endl;
+			cout << stack.peek() << endl;
 			break;
+
 		case 4:
-			cout << "The elements present in the Stacks are:" << endl;
-			displayStack();
-			cout << "Now deleting the Stack " << endl;
-			deleteStack();
-			cout << "End of program"<<endl;
+			cout << "Exiting the program" << endl;
+			break;
+		default:
+			cout << "Wrong option chosen, please select the correct option"<<endl;
+			break;
 		}
 	}
-	return 0;
-}
-/*=====End of Main =====*/
-
-/*=====Start of function definations =====*/
-int  pop()
-{
-	if (isEmpty())
-	{
-		cout << "Stack is Empty, No item to pop" << endl;
-		return -1;
-	}
-	else
-	{
-		stack.stackSize--;
-		return(stack.stackNode->nodeData);
-		
-	}
-}
-
-void push(int data)
-{
-	if (isEmpty())
-	{
-		stack.stackNode = (NodePtr)malloc(1 * (sizeof(struct Node)));
-	}
-	else
-	{
-		NodePtr new_node= (NodePtr)malloc(1 * (sizeof(struct Node)));
-		new_node->nextNodePtr = stack.stackNode;
-		stack.stackNode->nextNodePtr = new_node;
-	}
-	stack.stackNode->nodeData = data;
-	stack.stackSize++;
-
-}
-int  peek()
-{
-	if (isEmpty())
-	{
-		cout << "The Stack is empty"<<endl;
-		return -1;
-	}
-	else
-	{
-		return(stack.stackNode->nodeData);
-	}
-}
-
-bool isEmpty()
-{
-	return ((stack.stackSize==0) ? true : false);
-}
-
-void deleteStack()
-{
-	if ( !isEmpty()/*stack.stackNode != NULL*/)
-	{
-		NodePtr temp = stack.stackNode;
-		stack.stackNode = stack.stackNode->nextNodePtr;
-		stack.stackSize--;
-		cout << "Deleting data " << temp->nodeData;
-		free(temp);
-		deleteStack();
-	}
-}
-void displayStack(NodePtr nodePtr)
-{
-	if (isEmpty())
-	{
-		cout << "Stack is empty" << endl;
-	}
-	else
-	{
-		cout << " " << stack.stackNode->nodeData << endl;
-		displayStack(stack.stackNode->nextNodePtr);
-	}
-}
 
 
-/*=====End of function definations =====*/
+}
