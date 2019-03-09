7f6a119c0a3ec4cf86d0b98f3ec776c56448bf26
HoobyNoobyCoder <erenmotog5@gmail.com>
2019-03-09 20:41:58 +0530

    {Class in diff file }working basic Stack using linkedlist

--- VS_Datastructures_in_CPP/First_File/StackUsingLinkedList.cpp
+++ VS_Datastructures_in_CPP/First_File/StackUsingLinkedList.cpp
@@ -1,99 +1,8 @@
 #include "stdafx.h"
 #include<iostream>
 using namespace std;
+#include"First_File.cpp"
 
-class myStack {
-/* Structure for Linked List Node*/
-	struct Node
-	{
-		int nodeData;
-		struct Node* nextNodePtr=NULL;
-	};
-	typedef struct Node* NodePtr;
-
-public:
-/*Function Prototypes*/
-//	int pop();
-//	void push(int data);
-//	int peek();
-//	bool isEmpty();
-//	NodePtr createNode();
-
-/* Declarations */
-
-	NodePtr Top = NULL;
-/*Functions Definations*/
-
-/*Funtions to pop the top element from the stack*/
-	int pop()
-	{
-		if (!isEmpty())
-		{
-			int data;
-			NodePtr temp = Top;
-			Top = Top->nextNodePtr;
-			data = temp->nodeData;
-			free(temp);
-			return data;
-		}
-		else
-		{
-			cout << "Stack underflow, exiting program" << endl;
-			exit(1);
-		}
-	}
-/*Function to push the Data to the Stack*/
-	void push(int data)
-	{
-		if (isEmpty())
-		{
-			Top = createNode();
-			Top->nodeData = data;
-		}
-		else
-		{
-			NodePtr new_node = createNode();
-			new_node->nodeData = data;
-			new_node->nextNodePtr = Top;
-			Top = new_node;
-		}
-
-	}
-
-/*Function to check whether the stack is empty or not*/
-	bool isEmpty()
-	{
-		return(Top == NULL);
-	}
-/*Function to get the Top element without deleting it */
-	int peek()
- {
-	 if (isEmpty())
-		{
-		 cout << "Empty Stack, No element is pushed" << endl;
-		 return -1;
-		}
-	 else
-		 {
-		 return (Top->nodeData);
-		 }
- }
-/*Function to Create new Node */
-	NodePtr createNode()
-	{
-		NodePtr new_node = (NodePtr)malloc(1 * (sizeof(struct Node)));
-		if (new_node == NULL)
-		{
-			cout << "Memory Error in Malloc" << endl;
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			new_node->nextNodePtr = NULL;
-			return new_node;
-		}
-	}
-};
  char instructionString[] = "Choose \n '1' to Push \n '2' to Pop \n '3' to Peek \n '4' to exit \n";
 int main()
 {
@@ -132,6 +41,5 @@ int main()
 			break;
 		}
 	}
-
-
 }
+
